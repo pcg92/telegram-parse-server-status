a9b2ff76d6a053845710808d40b4eb5f6f58a728
pcegarra <fortunatus26@gmail.com>
2017-08-05 13:23:32 +0200

    indented++

--- telegram.js
+++ telegram.js
@@ -3,21 +3,20 @@ var kue = require('kue-scheduler');
 var Queue = kue.createQueue();
 
 function checkStatus(job, done){
-
-Parse.Cloud.httpRequest({
-  method: 'POST',
-  url: 'https://api.telegram.org/botYOUR_API_BOT/sendMessage?chat_id=YOUR_CHAT_ID',
-  params: {
-    text : 'Server is running.'
-  },
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  }
-}).then(function(httpResponse) {
-    done();
-}, function(httpResponse) {
-    done(new Error(httpResponse.status));
-});
+  Parse.Cloud.httpRequest({
+    method: 'POST',
+    url: 'https://api.telegram.org/botYOUR_API_BOT/sendMessage?chat_id=YOUR_CHAT_ID',
+    params: {
+      text : 'Server is running.'
+    },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  }).then(function(httpResponse) {
+      done();
+  }, function(httpResponse) {
+      done(new Error(httpResponse.status));
+  });
 }
 
 function initCheckStatus(){
